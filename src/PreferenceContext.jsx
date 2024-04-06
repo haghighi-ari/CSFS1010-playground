ca72f68e348d63ea5e2271879f405b966f8fa04d
mccooll <github@no3.ca>
2024-04-06 14:13:11 -0400

    add localstorage to context init

--- src/PreferenceContext.jsx
+++ src/PreferenceContext.jsx
@@ -1,9 +1,15 @@
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 
 const PreferenceContext = createContext({});
 
 function PreferenceProvider({ children }) {
   const [prefs,setPrefs]=useState({test:'test'})
+  
+  useEffect(() => {
+    const savedPrefs = localStorage.getItem('prefs')
+    if(savedPrefs) setPrefs(JSON.parse(savedPrefs))
+  },[])
+
   return <PreferenceContext.Provider value={{prefs,setPrefs}}>{children}</PreferenceContext.Provider>;
 }
 
